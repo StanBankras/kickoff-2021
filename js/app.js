a37a4d002306420b7c464a4c0ad7d66719a83bf5
Stan Bankras <stanbankras@gmail.com>
2021-02-01 14:01:51 +0100

    Visitekaartje versie 1

---
+++ js/app.js
@@ -0,0 +1,45 @@
+const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/'
+
+// GET REQUEST
+let me = {};
+
+const teams = fetch(`${url}/squads/1/teams/2/members`)
+  .then(response => response.json())
+  .then(data => {
+    me = data.find(d => d.id === "3");
+    document.title = document.title.replace('{NAAM}', `${me.name} ${me.surname}`);
+  });
+
+
+
+
+const putData = {
+  id: 3,
+  teamId: 2,
+  name: 'Stan',
+  prefix: '',
+  surname: 'Bankras',
+  mugshot: 'https://raw.githubusercontent.com/StanBankras/kickoff-2021/main/img/mugshot.jpg',
+  githubHandle: 'StanBankras',
+  other: {
+    sport: 'Wielrennen',
+    muziek: 'EDM',
+    werkplek: 'Thuis'
+  }
+}
+
+async function postData(url = '', data = {}) {
+  const response = await fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  return response.json();
+}
+
+// postData(`${url}/squads/1/teams/2/members/3`, putData)
+//   .then(data => {
+//     console.log('put', data);
+//   });
