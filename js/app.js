c15183c0e5c6e8998fd36e3fee5c2313bd3956b9
Stan Bankras <stanbankras@gmail.com>
2021-02-01 15:40:55 +0100

    Improve structure

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ const url = 'https://600ff44f6c21e1001704fac2.mockapi.io/minor-web/api/'
 // GET REQUEST
 let me = {};
 
-const teams = fetch(`${url}/squads/1/teams/2/members`)
+fetch(`${url}/squads/1/teams/2/members`)
   .then(response => response.json())
   .then(data => {
     me = data.find(d => d.id === "3");
